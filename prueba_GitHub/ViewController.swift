0f0ae4bbc4f1eb8db2b201a9c278470f89515c9d
Enrique Baca <EnriqueBaca@ebacalap.local>
2016-08-15 22:18:35 -0500

    segundo comentario de prueba

--- prueba_GitHub/ViewController.swift
+++ prueba_GitHub/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+//otro comentario porque el de alla abjo no lo cargo, no se porque!!😨
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
