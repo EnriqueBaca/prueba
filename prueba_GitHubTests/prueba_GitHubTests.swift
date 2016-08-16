9a2b00a49003322ebc644162f76e5d51aa2fb2d8
Enrique Baca <EnriqueBaca@ebacalap.local>
2016-08-15 22:09:28 -0500

    Initial Commit

---
+++ prueba_GitHubTests/prueba_GitHubTests.swift
@@ -0,0 +1,36 @@
+//
+//  prueba_GitHubTests.swift
+//  prueba_GitHubTests
+//
+//  Created by Luis Enrique Baca Ramirez on 15/08/16.
+//  Copyright © 2016 Luis Enrique Baca Ramirez. All rights reserved.
+//
+
+import XCTest
+@testable import prueba_GitHub
+
+class prueba_GitHubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
